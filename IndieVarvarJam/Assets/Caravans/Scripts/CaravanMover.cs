de4a1498d911142749baeb9158bdff542862146c
aigerimalmazbekkyzy <71431806+moonbell-e@users.noreply.github.com>
2022-02-27 16:25:53 +0300

    Merging scenes

--- IndieVarvarJam/Assets/Caravans/Scripts/CaravanMover.cs
+++ IndieVarvarJam/Assets/Caravans/Scripts/CaravanMover.cs
@@ -14,9 +14,12 @@ public class CaravanMover : MonoBehaviour
 
     private int _currentWayPointIndex;
 
+    private Transform _transform;
+
     private void Awake()
     {
-        _speed = 5f;
+        _speed = 1f;
+        _step = 0.05f;
         List<Vector3> curvePoints = new List<Vector3>();
         foreach (Transform trf in _curvePoints)
             curvePoints.Add(trf.position);
@@ -25,18 +28,22 @@ public class CaravanMover : MonoBehaviour
         {
             _wayPoints.Add(_pathGenerator.BieseCurvePoint(i, curvePoints));
         }
+
+        _transform = transform;
+        transform.position = _wayPoints[0];
+
     }
 
     private void Update()
     {
-        if(Vector3.Distance(transform.position, _wayPoints[_currentWayPointIndex]) < 0.01f)
+        if(Vector3.Distance(_transform.position, _wayPoints[_currentWayPointIndex]) < 0.05f)
         {
             _currentWayPointIndex++;
             return;
         }
         else
         {
-            transform.position += (_wayPoints[_currentWayPointIndex] - transform.position) * _speed * Time.deltaTime;
+            _transform.position += (_wayPoints[_currentWayPointIndex] - _transform.position) * _speed * Time.deltaTime;
         }
     }
 }
